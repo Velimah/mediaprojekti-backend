d59b81fcac2cc1deb43f7ce1c54c0651d7260de6
nikoprl <45226198+nikoprl@users.noreply.github.com>
2023-11-22 21:27:19 +0200

    Add login route

--- routes/user-routes.ts
+++ routes/user-routes.ts
@@ -3,6 +3,7 @@ import { MongoError } from "mongodb";
 import bcrypt from "bcrypt";
 import { User } from "../models/user-model";
 import dotenv from "dotenv";
+import jwt, { Secret } from "jsonwebtoken";
 dotenv.config();
 const router = express.Router();
 
@@ -35,6 +36,46 @@ router.post("/register", async (req: Request, res: Response) => {
       return res
         .status(500)
         .json({ message: "Register error: ", error: String(error) });
+    }
+  }
+});
+
+const jwtsecret: Secret = process.env.JWT_SECRET as string;
+
+const generateToken = (user: { username: string }) => {
+  return jwt.sign({ username: user.username }, jwtsecret, { expiresIn: "24h" });
+};
+
+// Login user
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if password matches
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // JWT token
+    const token = generateToken({ username: user.username });
+
+    return res.status(200).json({ message: "Login successful", token });
+  } catch (error: unknown) {
+    if (error instanceof MongoError) {
+      return res
+        .status(500)
+        .json({ message: "MongoError: ", error: error.message });
+    } else {
+      return res
+        .status(500)
+        .json({ message: "Login failed", error: String(error) });
     }
   }
 });
